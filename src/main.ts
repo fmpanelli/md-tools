48174422e15d42977228416b3977dfc15483317d
Filippo Maria Panelli <UT12187@GROUP.CREDEM.NET>
2025-05-25 23:43:59 +0200

    Add main.js

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { exit } from "node:process";
+import { extractPngsFromFile } from "./processFile";
+
+const cliArgs = process.argv.slice(2);
+if (cliArgs.length !== 2) {
+  console.error("Syntax: mdunbundle input-file output-folder");
+  exit(1);
+}
+
+const inputFile = cliArgs[0];
+const outputFolder = cliArgs[1];
+
+extractPngsFromFile(inputFile, outputFolder).then(() => {
+  exit(0);
+});

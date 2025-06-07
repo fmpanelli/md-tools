b9ca0d0af699892704c8515e51bd8cdc089fad12
Filippo Maria Panelli <UT12187@GROUP.CREDEM.NET>
2025-06-07 20:21:34 +0200

    Improve npx commands

--- src/unbundle.ts
+++ src/unbundle.ts
@@ -1,9 +1,21 @@
 #!/usr/bin/env node 
-
-import { exit } from "node:process";
-
-  console.log("@fmpanelli/md-tools");
-  console.log("Various tools and utilities for markdown.\n");
-  console.log("Invoke the utilities as follows:");
-  console.log("- Unbundle: npx --package=@fmpanelli/md-tools unbundle <input-file> <output-folder>");
-  exit(0);
+  
+  import { exit } from "node:process";
+  import { extractPngsFromFile } from "./processFile";
+  
+  const cliArgs = process.argv.slice(2);
+  if (cliArgs.length !== 2) {
+    console.error("Syntax: npx --package=@fmpanelli/md-tools unbundle <input-file> <output-folder>");
+    exit(1);
+  }
+  
+  const inputFile = cliArgs[0];
+  const outputFolder = cliArgs[1];
+  
+  extractPngsFromFile(inputFile, outputFolder).then(() => {
+    exit(0);
+  }).catch((err) => {
+    console.error(err);
+    exit(1);
+  });
+  

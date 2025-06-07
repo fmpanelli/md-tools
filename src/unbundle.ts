ce8bdd981b9affc8cbe1b76176a4328522f241ad
Filippo Maria Panelli <UT12187@GROUP.CREDEM.NET>
2025-06-07 19:42:45 +0200

    Separate invokation of main from unbundle

---
+++ src/unbundle.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node 
+
+import { exit } from "node:process";
+
+  console.log("@fmpanelli/md-tools");
+  console.log("Various tools and utilities for markdown.\n");
+  console.log("Invoke the utilities as follows:");
+  console.log("- Unbundle: npx --package=@fmpanelli/md-tools unbundle <input-file> <output-folder>");
+  exit(0);
